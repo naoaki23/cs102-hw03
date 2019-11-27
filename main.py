453dfb8a1af20cc49354f546b2b61b528a3d3be0
naoaki23 <naomi.akiyama@cooper.edu>
2019-11-27 10:47:56 -0500

    MeanCalcNaomi

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-
+from statistics import mean
 
 def main():
     assert len(sys.argv) > 1, "No input file path specified."
@@ -8,6 +8,15 @@ def main():
     print(f"Processing input file: {input_file_path}")
 
     # TODO: Fill in the actual logic here!
+    with open(input_file_path, "r") as f:
+       for line in f:
+           line = line.strip("\n")
+           to_calc = line.split(",")
+           num_list = []
+           for n in to_calc:
+               num_list.append(float(n))
+           av = mean(num_list)
+           print(f"{av}")
 
 
 if __name__ == "__main__":
